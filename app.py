1b05dde94e7927e1c0f3db4e76084bd71f0cb609
Валерий Газаров <v.gazarov@fortus.pro>
2022-01-24 15:59:27 +0300

    Change Config.Development to env var

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +8,6 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
