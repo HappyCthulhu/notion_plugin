4e239706a02db52ef76d174ed2335a25bfea3344
Валерий Газаров <v.gazarov@fortus.pro>
2022-01-30 18:14:15 +0300

    Add check for db exists

--- app.py
+++ app.py
@@ -16,4 +16,10 @@ from flask_migrate import Migrate
 
 migrate = Migrate(app, db)
 
-import models
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+engine = create_engine(os.environ['DATABASE_URL'])
+if not database_exists(engine.url):
+    create_database(engine.url)
+print(database_exists(engine.url))
