e313f292b184133c891b51bb1b39b82d0a8e7005
Валерий Газаров <v.gazarov@fortus.pro>
2022-03-27 17:14:25 +0300

    Add app_context() in queries to db

--- some_tests/unit_tests/help_funcs_for_tests.py
+++ some_tests/unit_tests/help_funcs_for_tests.py
@@ -1,16 +1,17 @@
 import time
 from datetime import datetime
 
-from app import db
-from backend.models import AllNotionPages
+from app import db, app
 from backend.helpers.parse_bookmarks import parse_bookmarks
+from backend.models import AllNotionPages
 
 
 def check_present_of_record_in_db_all_notion_pages(title):
-    if db.session.query(AllNotionPages).filter_by(title=title).all():
-        return True
-    else:
-        return False
+    with app.app_context():
+        if db.session.query(AllNotionPages).filter_by(title=title).all():
+            return True
+        else:
+            return False
 
 
 def find_bookmark_by_title(title):
