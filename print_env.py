145313af68c22887aa661ab8ca869df7ce90afae
Валерий Газаров <v.gazarov@fortus.pro>
2022-05-22 10:19:23 +0300

    Delete this

--- print_env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-try:
-    if os.environ['ROOT_PATH_DIR']:
-        print('I can access to env vars!')
-    else:
-        print('Env var in empty... But, at least i have access to env vars!')
-except:
-    print('Cant get access to env vars((')
